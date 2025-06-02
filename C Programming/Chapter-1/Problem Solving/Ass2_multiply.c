400a3be3a3ab43a230bafc3b89ebd979dc19381e
DurjoyKumar177 <durjoykumar177@gmail.com>
2025-06-02 21:29:52 +0600

    solution of some HackerRank problem are given

---
+++ C Programming/Chapter-1/Problem Solving/Ass2_multiply.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+    long long int a,b;
+    long long int multiply;
+    scanf("%lld%lld",&a,&b);
+
+    multiply= (a * b);
+
+    printf("%lld",multiply);
+
+    return 0;
+}
+
