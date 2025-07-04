50d387ad06a9bd5c303b2f4227d4658b13ca9e44
DurjoyKumar177 <durjoykumar177@gmail.com>
2025-07-04 22:54:15 +0600

    fix code problem

--- C Programming/Chapter-1/2_Conditionals Statments with Arithmatic operations/continue.c
+++ C Programming/Chapter-1/2_Conditionals Statments with Arithmatic operations/continue.c
@@ -5,11 +5,11 @@ int main()
     for(i=1;i<=10;i=i+1)
     {
         
-        printf("%d\n",i);
         if(i==5)
         {
             continue;
         }
+        printf("%d\n",i);
     }
     return 0;
 }
