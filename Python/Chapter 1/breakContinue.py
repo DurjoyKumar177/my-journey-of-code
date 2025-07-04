c5ea0bd5b01eebe74b1827a1302d285e48ef9dec
DurjoyKumar177 <durjoykumar177@gmail.com>
2025-07-05 00:03:18 +0600

    update break and continue code

--- Python/Chapter 1/breakContinue.py
+++ Python/Chapter 1/breakContinue.py
@@ -1,6 +1,16 @@
 n=10
 
+#Break Statement
+print("Break Statement: ")
 for i in range(n+1):
     if i==5:
-        break
+        break #stop printing after 4
     print(i)
+
+print("\n\nContinue Statement: ")    
+#Continue Statement:
+for i in range(n):
+    if i==5:
+        continue
+    print(i)    
+    
