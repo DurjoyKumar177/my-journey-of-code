7786bb5f33e495b405b4f950ab28c2f23b282274
DurjoyKumar177 <durjoykumar177@gmail.com>
2025-07-04 22:59:23 +0600

    code for Break and Continue statement added

---
+++ Python/Chapter 1/Conditional_statement.py
@@ -0,0 +1,13 @@
+a=int (input('Enter the Number: '))
+
+if a < 5:
+    print("Small Number")
+    
+elif a > 10:
+    print("Big Number")
+    
+else:
+    print("The Number is Between 5 to 10.")
+    
+
+    
---
+++ Python/Chapter 1/breakContinue.py
@@ -0,0 +1,6 @@
+n=10
+
+for i in range(n+1):
+    if i==5:
+        break
+    print(i)
