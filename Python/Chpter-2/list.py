3d26b2e89c44bd339fed5f9521ca65e3bb1b55b0
DurjoyKumar177 <durjoykumar177@gmail.com>
2025-07-05 18:56:51 +0600

    code for python list added

---
+++ Python/Chpter-2/list.py
@@ -0,0 +1,17 @@
+#list,array,collection is same(simple terms)
+
+numbers=[45,56,12,89,87,32,47,45,23]
+#indes =0   1  2   3 4   5  6  7  8
+#reversind=-9  -8  -7  -6  -5  -4  -3  -2  -1
+
+print(numbers[3], numbers[-3])
+
+#list (start: end)
+print(numbers[2:6])
+
+#list(start:end:step)
+print(numbers[0:6:2])
+print(numbers[6:3:-1])#going reverse
+print(numbers[:])#take all
+print(numbers[::-1])#short cut to reverse
+
