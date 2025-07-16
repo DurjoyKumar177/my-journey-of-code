0a372b8201288eb0c6528ea2a8233ec1217076ea
DurjoyKumar177 <durjoykumar177@gmail.com>
2025-07-16 17:44:47 +0600

    Basic input output program for C++ are added

---
+++ C++/Chapter -1/Input output EOF and SetPrecision/input_Output.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;// diclare first to mentionaing std in input or output in main function
+
+int main()
+{
+    int a;
+    float b;
+    char c;
+    cin>> a>>b>>c; //c++ does not need formate spacifier like %d,%c%lf it determaind it automatically
+    cout<<a << " "<< b<< " "<< c << " "<<endl;
+
+    return 0;
+}
