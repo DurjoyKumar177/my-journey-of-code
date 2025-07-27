b69120d527c8cd826438e9873634c0d5040b9989
DurjoyKumar177 <durjoykumar177@gmail.com>
2025-07-27 23:56:28 +0600

    add word before string

---
+++ C++/Chapter -3/int_before_string.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+int main()
+{
+    int x;
+    cin>>x;
+    cin.ignore();//to avoid a character;
+    //getchar();we can also use that;
+    string s;
+    getline(cin,s);
+    return 0;
+}
