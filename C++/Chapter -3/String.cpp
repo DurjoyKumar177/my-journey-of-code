76e42814a4e1f6fe5af53580c577f2756fc76833
DurjoyKumar177 <durjoykumar177@gmail.com>
2025-07-27 23:44:39 +0600

    ADD Chapter 3 for String operations in C++

---
+++ C++/Chapter -3/String.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>//all header file was include here but it consume more time 
+#include<string>//for string in cpp but no need to use if use bits/stdc++.h
+
+using namespace std;
+int main()
+{
+    string s1="hello";
+    string s2="hello how are you";
+    cout<<s1<<s2<<endl;
+    return 0;
+}
