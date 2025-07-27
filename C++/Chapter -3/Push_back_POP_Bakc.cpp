b675fd5d17ea9f1497f2d62813667e59ba11503b
DurjoyKumar177 <durjoykumar177@gmail.com>
2025-07-27 23:56:52 +0600

    push and pop back in string

---
+++ C++/Chapter -3/Push_back_POP_Bakc.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+//push back generally used to add character in memory
+//pop_back used to delete character in memory 
+int main()
+{
+    string a="Hello";
+    string b="world";
+    a=a+'A';
+    cout<< a<<endl;
+    a="HelloA";//also work asme;
+    a.push_back('A');//increase memory size and add character;
+    cout<<a<<endl;
+    a.pop_back();//remove one character ;
+    cout<<a<<endl;
+
+    return 0;
+}
