a3f9e7ebc3b0863f4f315d09ad7a614e959d1df7
DurjoyKumar177 <durjoykumar177@gmail.com>
2025-07-28 23:45:52 +0600

    code for ranged basedd for loop

---
+++ C++/Chapter -3/ranged_based_for_loop.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    cin >> s;
+    // for(int i=0;i<s.size();i++)
+    // {
+    //     cout<<s[i]<<endl;
+    // }
+    for (char c : s) // এখানে s ar sob vlue ak ak kore c variable a jacche abong print hocche;
+    {
+        cout << c << endl;
+    }
+    return 0;
+}
