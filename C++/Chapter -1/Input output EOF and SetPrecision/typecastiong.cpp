da4dd9036a1e35c63d1bbf240085048e43708055
DurjoyKumar177 <durjoykumar177@gmail.com>
2025-07-29 12:13:16 +0600

    modify type casting code in c++

--- C++/Chapter -1/Input output EOF and SetPrecision/typecastiong.cpp
+++ C++/Chapter -1/Input output EOF and SetPrecision/typecastiong.cpp
@@ -1,3 +1,5 @@
+//Typecasting in C++ refers to the process of converting a variable from one data type to another.
+
 #include<iostream>
 
 using namespace std;
@@ -6,10 +8,10 @@ int main()
 {
     char c;
     cin>>c;
-    int acchi= c;
+    int acchi= c; // acchi is the ASCII value of the character c
     cout<< c << " " << acchi <<endl;
     // we also used
-    cout<< int(c)<<endl;
+    cout<< int(c)<<endl; //another way for showing the ASCII value of the character c
 
     long long int b=125525;
     
