07756979dd8e6bef8b3f424c44f15ae6bbd57109
DurjoyKumar177 <durjoykumar177@gmail.com>
2025-07-30 08:51:50 +0600

    Method of taking string after a int veriable.

--- C++/Chapter -3/int_before_string.cpp
+++ C++/Chapter -3/int_before_string.cpp
@@ -8,6 +8,10 @@ int main()
     cin.ignore();//to avoid a character;
     //getchar();we can also use that;
     string s;
-    getline(cin,s);
+    getline(cin,s);//getline is used to take a string with space;
+    //cin>>s; //this will not take the space;
+    cout<<endl;
+    cout<<"Given String is: "<<s<<endl;
+    cout<<"Given Integer: " <<x<<endl;
     return 0;
 }
